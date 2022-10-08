e843a92df197a044fcacb75f0d545b978af03335
Jesus Cristancho <jesusmanuelcp@ufps.edu.co>
2022-10-07 22:16:38 -0500

    imagenes dinamicas

--- js/funciones.js
+++ js/funciones.js
@@ -1,74 +1,71 @@
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+.then((response) => response.json())
+.then((data) => filltable(data));
+
 function filltable(data){
-        //console.log(data)
-        var cardguia = document.querySelector("cardguia");
-
-        for (let json in data) {
-          let object= data[json];
-          console.log(object);
-
-          for(let drink in object){
-
-            let img =object[drink]["strDrinkThumb"];
-            let nombre = object[drink]["strDrink"];
-            let alcohol =object[drink]["strAlcoholic"];
-            let str = 1;
-            let ingredientes=[];
-            console.log(ingredientes);
-            let ingrediente = "strIngredient"+ str +"";
-            while (str<=15&&object[drink][ingrediente]!==null) {
-
-
-              ingredientes.push(object[drink][ingrediente]);
-              ingrediente = "strIngredient"+ str +"";
-              str++;
-
-            }
-            console.log(ingredientes);
-        }
-
-          /*
-
-           let id_de_caso = object.id_de_caso;
-          let dto=object.departamento_nom;
-          let edad=object.edad;
-          let sexo=object.sexo;
-          let e=object.recuperado;
-          console.log(e);
-          let row = table.insertRow()
-
-
-
-                    let cell_id_de_caso = row.insertCell()
-                    cell_id_de_caso.innerHTML = id_de_caso
-
-                    let cell_departamento_nom = row.insertCell()
-                    cell_departamento_nom.innerHTML = dto
-
-                    let cell_sexo = row.insertCell()
-                    cell_sexo.innerHTML = sexo
-
-                    let cell_edad = row.insertCell()
-                    cell_edad.innerHTML = edad
-                    let cell_fecha_reporte_web = row.insertCell()
-                    cell_fecha_reporte_web.innerHTML = e
-
-                    let cell_estado = row.insertCell()
-                    */
-        }
+  //console.log(data)
+  var cardguia = document.querySelector("cardguia");
+  var card ;
+  //container.insertAdjacentElement('beforeend',card);
+  for (let json in data) {
+    let object= data[json];
+    console.log(object);
+    for(let drink in object){
+      let img =object[drink]["strDrinkThumb"];
+      let name = object[drink]["strDrink"];
+      let alcohol =object[drink]["strAlcoholic"];
+      let str = 1;
+      let ingredientes=[];
+      //console.log(ingredientes);
+      let ingrediente = "strIngredient"+ str +"";
+      while (str<=15&&object[drink][ingrediente]!==null) {
+        ingredientes.push(object[drink][ingrediente]);
+        ingrediente = "strIngredient"+ str +"";
+        str++;
       }
+      card = crearCard(drink, img);
 
-
-      fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-      .then((response) => response.json())
-      .then((data) => filltable(data));
-
-
-class cocktel {
-  constructor(drink,num) {
-    this.imagen=drink[num]["strDrinkThumb"];
-    //this.nombre=nombre;
-    //this.ingredientes= ingrediente(ingredientes);
-    //this.alcohol=alcohol;
-
+      //container.insertAdjacentElement("beforeend",card);
+      //console.log(ingredientes);
+    }
   }
+}
+function crearCard(i, image){
+
+  let card = cardguia.cloneNode();
+  let img =imgcocktel.cloneNode();
+  let bodycard1 = bodycard.cloneNode();
+    let titulococktel1 = titulococktel.cloneNode();
+    let ingredientes1 = ingredientes.cloneNode();
+    let listingredientes1= listingredientes.cloneNode();
+      let ingrediente1= ingrediente.cloneNode();
+    let cardfooter1 =cardfooter.cloneNode();
+      let alcohol1 = alcohol.cloneNode();
+      let comprar1 = comprar.cloneNode();
+
+      card.setAttribute("id","card"+ i)
+      img.setAttribute('id','imgcocktel' + i);
+      bodycard1.setAttribute('id','bodycard' + i);
+      titulococktel1.setAttribute('id','titulococktel' + i);
+      ingredientes1.setAttribute('id','ingredientes' + i);
+      listingredientes1.setAttribute('id','listingredientes' + i);
+      ingrediente1.setAttribute('id','ingrediente' + i);
+      cardfooter1.setAttribute('id','cardfooter' + i);
+      alcohol1.setAttribute('id','alcohol' + i);
+      comprar1.setAttribute('id','comprar' + i);
+
+      img.setAttribute('src', image);
+
+
+    cardfooter1.insertAdjacentElement('beforeend',alcohol1);
+    cardfooter1.insertAdjacentElement('beforeend',comprar1);
+      listingredientes1.insertAdjacentElement('beforeend',ingrediente1);
+    bodycard1.insertAdjacentElement('beforeend',titulococktel1);
+    bodycard1.insertAdjacentElement('beforeend',ingredientes1);
+    bodycard1.insertAdjacentElement('beforeend',listingredientes1);
+    card.insertAdjacentElement('beforeend',img);
+    card.insertAdjacentElement('beforeend',bodycard1);
+    card.insertAdjacentElement('beforeend',cardfooter1);
+    container.insertAdjacentElement('beforeend',card);
+    return card;
 }
